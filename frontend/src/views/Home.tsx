6b0c2c1145b6ca05c971718fdc21e49528d5a395
Hasham M Khan <hasham.m.khan@outlook.com>
2022-10-29 12:07:30 -0500

    added Home view

--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -1,6 +1,27 @@
+import './Home.sass'
+
+import Header from '../components/Header/Header'
+import About from '../components/About/About'
+
 function Home () {
   return (
-    "hi"
+    <div>
+    <Header />
+    <main>
+      <About />
+      <section className="case-studies">
+        <h2>The proof is in our work</h2>
+        <p>
+          For over 10 years, Dialexa has partnered with industry-leading 
+          companies to create innovative technology. We want you to be our next 
+          great success story.
+        </p>
+        <div>
+          <a href="#">view our case studies</a>
+        </div>
+      </section>
+    </main>
+    </div>
   )
 }
 
