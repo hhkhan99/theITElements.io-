41a24749b36cb8de785cbea5c48814c5d3bc8a03
Hasham M Khan <hasham.m.khan@outlook.com>
2022-10-29 12:08:39 -0500

    updated About to include AboutCard

--- frontend/src/components/About/About.tsx
+++ frontend/src/components/About/About.tsx
@@ -1,7 +1,14 @@
 import './About.sass';
-import robot from '../../images/lp/robot.svg'
+import AboutCard from './AboutCard/AboutCard'
 
-export default function AboutCard () {
+import robot from '../../images/lp/robot.svg'
+import ui from '../../images/lp/ui.svg'
+import se from '../../images/lp/software.svg'
+import ds from '../../images/lp/data-science.svg'
+import ts from '../../images/lp/technology.svg'
+import he from '../../images/lp/hardware.svg'
+
+export default function About () {
   const cardImgs = [
     'hardware', 'platform', 'software', 'technology', 
     'ui', 'data-science'
@@ -18,7 +25,12 @@ export default function AboutCard () {
         <div className="line"></div>
       </section>
       <section className="about-cards">
-
+        <AboutCard title="user experience design" img={ ui }></AboutCard>
+        <AboutCard title="custom software engineering" img={ se }></AboutCard>
+        <AboutCard title="data science" img={ ds }></AboutCard>
+        <AboutCard title="technology strategy" img={ ts }></AboutCard>
+        <AboutCard title="hardware engineering" img={ he }></AboutCard>
+        <div className="line"></div>
       </section>
     </section>
   )
