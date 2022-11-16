4e110aa079b58664858a63e7a75b1017d96bb76e
Hasham M Khan <hasham.m.khan@outlook.com>
2022-11-15 20:43:00 -0600

    added footer

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,5 +1,12 @@
 import './Footer.sass'
 
+import linkedinIcon from '../../images/icons/linkedin.svg'
+import twitterIcon from '../../images/icons/twitter.svg'
+import instagramIcon from '../../images/icons/instagram.svg'
+import facebookIcon from '../../images/icons/facebook.svg'
+import youtubeIcon from '../../images/icons/youtube.svg'
+import glassdoorIcon from '../../images/icons/glassdoor.svg'
+
 function Footer () {
 
   return (
@@ -15,7 +22,52 @@ function Footer () {
 
       <section className="contact">
         <h2>Get in touch</h2>
-        
+        <section className="contact-phone">
+          <h3>Phone:</h3>
+          <a href="tel:+18006145555">1(800) 614-5555</a>
+        </section>
+        <section className="contact-email">
+          <h3>Email:</h3>
+          <a href="mailto:hello@itelements.com">hello@itelements.com</a>
+        </section>
+        <section className="contact-address">
+          <span>Dallas, TX | Chicago, IL</span>
+        </section>
+      </section>
+
+      <hr />
+
+      <section className="copyright">
+        <span>
+          <small>&copy;</small> 2022 Diarexa, LLC. All rights reserved.
+        </span>
+      </section>
+
+      <section className="footer-social">
+        <a href="#">
+          <img src={ linkedinIcon } alt="linkedin" />
+        </a>
+        <a href="#">
+          <img src={ twitterIcon } alt="Twitter" />
+        </a>
+        <a href="#">
+          <img src={ instagramIcon } alt="Instagram" />
+        </a>
+        <a href="#">
+          <img src={ facebookIcon } alt="Facebook" />
+        </a>
+        <a href="#">
+          <img src={ youtubeIcon } alt="Youtube" />
+        </a>
+        <a href="#">
+          <img src={ glassdoorIcon } alt="Glassdoor" />
+        </a>
+      </section>
+
+      <section className="footer-links">
+        <a href="#">Privacy Policy</a>
+        <a href="#">Terms &amp; Conditions</a>
+        <a href="#">Cookie Preferences</a>
       </section>
 
     </footer>
